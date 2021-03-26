249fb18f49acfb84772121dcc75cc75676dc3db4
ginesta-basart <64841058+ginesta-basart@users.noreply.github.com>
2021-03-26 19:26:25 +0100

    Implementación de la generadora de soluciones y arreglos de la definición del estado

--- aima/src/probRedSensors/DefinicionEstado.java
+++ aima/src/probRedSensors/DefinicionEstado.java
@@ -1,27 +1,47 @@
 package probRedSensors;
 
+//Author: Ginesta Basart
+
 public class DefinicionEstado {
 	
 	//Implementacion del estado
-	private static int dim;
+	private static int numSensores;
+	private static int numCentros;
 	private int[][] estado;
 	
 	public DefinicionEstado(int s, int c){		
-		dim = s+c;
+		int dim = s+c;
 		estado = new int[dim][dim];
+		numSensores = s;
+		numCentros = c;
+		
 	}
 	
 	public int[][] actual(){
 		return estado;
 	}
 	
-	public void asignaEstadoActual(int[][] nuevoActual) {
-		estado = nuevoActual;
+	public int size() {
+		return numSensores+numCentros;
 	}
 	
-	//Necesario para comprovar la restriccion de conexiones
+	public int numSensores() {
+		return numSensores;
+	}
+	
+	public int numCentros() {
+		return numCentros;
+	}
+	
+	/*public void asignaEstadoActual(int[][] nuevoActual) {
+		estado = nuevoActual;
+	}*/
+	
+	
 	public int sumaConexiones(int elem) {
+		//Necesario para comprovar la restriccion de conexiones
 		int suma = 0;
+		int dim = numSensores+numCentros;
 		for(int i = 0; i < dim; i++ ) {
 			suma += estado[elem][i];
 		}
@@ -29,11 +49,15 @@ public class DefinicionEstado {
 	}
 	
 	//Implementacion de los operadores conectar-desconectar
+	
 	public void nuevaConexion(int elem1, int elem2) {
 		//canvia un bit 0 del estado a 1
 		if (elem1 == elem2) System.out.print("No.");
 		else {
-			if (estado[elem1][elem2] == 0) estado[elem1][elem2] = 1;
+			if (estado[elem1][elem2] == 0 && estado[elem2][elem1] == 0) {
+				estado[elem1][elem2] = 1;
+				estado[elem2][elem1] = 1;
+			}
 			else System.out.print("No.");
 		}
 		
@@ -43,7 +67,10 @@ public class DefinicionEstado {
 		//canvia un bit 1 del estado a 0
 		if (elem1 == elem2) System.out.print("No.");
 		else {
-			if (estado[elem1][elem2] == 1) estado[elem1][elem2] = 0;
+			if (estado[elem1][elem2] == 1 && estado[elem2][elem1] == 1) {
+				estado[elem1][elem2] = 0;
+				estado[elem2][elem1] = 0;
+			}
 			else System.out.print("No.");
 		}
 	}
--- aima/src/probRedSensors/GeneradorSolucionInicial.java
+++ aima/src/probRedSensors/GeneradorSolucionInicial.java
@@ -1,10 +1,64 @@
 package probRedSensors;
 
+//Author: Ginesta Basart
+
 public class GeneradorSolucionInicial {
 
+	
+	public GeneradorSolucionInicial(DefinicionEstado e) {
+		
+	}
+	
 	//Implementa la generacion de la solucion de partida para hill climbing
-	public GeneradorSolucionInicial() {
+	
+	public void generaSolucionInicial1(DefinicionEstado e) {
+		//Reparte los sensores entre los centros de datos
+		
+		int numCentros = e.numCentros();
+		int numSensores = e.numSensores();
+		
+		int div = numSensores/numCentros;
+		int mod = numSensores%numCentros;
+		
+		if (numSensores - 25*div > mod/numSensores/2) {
+			System.out.print("No empezamos bien: Hay demasiados sensores, y no se pueden respetar las restricciones de conexion.");
+		}
+		else {
+			
+			for (int i = 0; i < numCentros; i++) {
+				for (int j = 0; j < div; j++) {
+					e.nuevaConexion(numSensores+i, j+div*i); //offset de sensores ya asignados a un centro de datos
+					if(e.sumaConexiones(numSensores+i) > 25) {
+						System.out.print("No vamos nada bien: los sensores no se estan repartiendo entre los centros de datos respetando la limitación de conexiones.");
+						mod = 0;
+						j = div;
+						i = numCentros;
+					}
+				}
+			}
+			
+			if (mod != 0) {
+				//hay más de 25*numCentros sensores. Hay que repartirlos entre los sensores que ya estan conectados a algun centro (solo tendran una conexion)
+				for (int i = 0; i < mod; i++) {
+					for (int j = 0; 25*numCentros+1+i <= numSensores && j < 2; j++) {
+						//los asignamos de dos en dos para llegar al tope de conexiones posibles con los sensores ya concectados
+						e.nuevaConexion(25*numCentros+1+i, j+2*i);
+						if (e.sumaConexiones(25*numCentros+1+i) > 3) {
+							System.out.print("No vamos nada bien: los sensores restantes no se estan repartiendo entre los sensores asignados respetando la limitación de conexiones.");
+							j = 2;
+							i = mod;
+						}
+					}
+				}
+			}
+		}
+		
+	}
+	
+	public void generaSolucionInicial2(DefinicionEstado e) {
 		
 		
 	}
+	
+	
 }
