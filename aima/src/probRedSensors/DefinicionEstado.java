ae3938f64d2ac5ad151a4c8fd54291bdeb809c44
ginesta-basart <64841058+ginesta-basart@users.noreply.github.com>
2021-03-26 17:33:57 +0100

    Implementación de la estructura del estado y las operaciones.

--- aima/src/probRedSensors/DefinicionEstado.java
+++ aima/src/probRedSensors/DefinicionEstado.java
@@ -3,10 +3,11 @@ package probRedSensors;
 public class DefinicionEstado {
 	
 	//Implementacion del estado
+	private static int dim;
 	private int[][] estado;
 	
 	public DefinicionEstado(int s, int c){		
-		int dim = s+c;
+		dim = s+c;
 		estado = new int[dim][dim];
 	}
 	
@@ -14,14 +15,37 @@ public class DefinicionEstado {
 		return estado;
 	}
 	
+	public void asignaEstadoActual(int[][] nuevoActual) {
+		estado = nuevoActual;
+	}
 	
-	//Implementacion de los operadores
-	public void nuevaConexion(/*dos sensores, dos centros de datos o un sensor y un centro de datos*/) {
+	//Necesario para comprovar la restriccion de conexiones
+	public int sumaConexiones(int elem) {
+		int suma = 0;
+		for(int i = 0; i < dim; i++ ) {
+			suma += estado[elem][i];
+		}
+		return suma;
+	}
+	
+	//Implementacion de los operadores conectar-desconectar
+	public void nuevaConexion(int elem1, int elem2) {
 		//canvia un bit 0 del estado a 1
+		if (elem1 == elem2) System.out.print("No.");
+		else {
+			if (estado[elem1][elem2] == 0) estado[elem1][elem2] = 1;
+			else System.out.print("No.");
+		}
+		
 	}
 	
-	public void eliminaConexion(/*dos sensores, dos centros de datos o un sensor y un centro de datos*/) {
+	public void eliminaConexion(int elem1, int elem2) {
 		//canvia un bit 1 del estado a 0
+		if (elem1 == elem2) System.out.print("No.");
+		else {
+			if (estado[elem1][elem2] == 1) estado[elem1][elem2] = 0;
+			else System.out.print("No.");
+		}
 	}
-
+	
 }
