109ef7a91b50600fe8a17be8db63b0ba5abe21e3
ginesta-basart <64841058+ginesta-basart@users.noreply.github.com>
2021-03-27 21:36:45 +0100

    Esqueleto de la generacion de sucesores

--- aima/src/probRedSensors/GeneradorSucesoresHillClimbing.java
+++ aima/src/probRedSensors/GeneradorSucesoresHillClimbing.java
@@ -9,8 +9,22 @@ public class GeneradorSucesoresHillClimbing {
 	}
 	
 	//Implentacion de la generacion de sucesores para Hill Climbing
-	public ArrayList<DefinicionEstado> sucesores(DefinicionEstado actual) {
+	public ArrayList<DefinicionEstado> sucesores(DefinicionEstado actual, int v) {
+		//v es el valor del paquete a transmitir!!!!!!!!!!
+		
 		ArrayList<DefinicionEstado> r = new ArrayList<DefinicionEstado>();
+		FuncionHeuristica heuristico = new FuncionHeuristica();
+		int h_actual = heuristico.funcionHeuristica1(actual, v);
+		
+		for(int i = 0; i<actual.size(); i++) {
+			for(int j = i+1; j > actual.size(); j++) {
+				DefinicionEstado hijo = actual;
+				
+				
+				
+				r.add(hijo);
+			}
+		}
 		
 		return r;
 	}
